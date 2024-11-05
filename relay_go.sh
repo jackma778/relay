40b8a12129eb0351af6a5b81ff497ee5ef9378dc
jackma778 <36823039+jackma778@users.noreply.github.com>
2024-11-05 16:25:32 +0900

    Update relay_go.sh

--- relay_go.sh
+++ relay_go.sh
@@ -9,29 +9,33 @@ if ! [[ "$relay_node_id" =~ ^[0-9]+$ ]]; then
     exit 1
 fi
 # 优化内核参数 检查是否存在重复配置项
-if ! grep -q "net.ipv4.tcp_congestion_control = bbr" /etc/sysctl.conf; then
-    echo "net.ipv4.tcp_retries2 = 8
-    net.ipv4.tcp_slow_start_after_idle = 0
-    fs.file-max = 1000000
-    net.core.default_qdisc = fq
-    net.ipv4.tcp_congestion_control = bbr
-    fs.inotify.max_user_instances = 8192
-    net.ipv4.tcp_syncookies = 1
-    net.ipv4.tcp_fin_timeout = 30
-    net.ipv4.tcp_tw_reuse = 1
-    net.ipv4.ip_local_port_range = 1024 65000
-    net.ipv4.tcp_max_syn_backlog = 16384
-    net.ipv4.tcp_max_tw_buckets = 6000
-    net.ipv4.route.gc_timeout = 100
-    net.ipv4.tcp_syn_retries = 1
-    net.ipv4.tcp_synack_retries = 1
-    net.core.somaxconn = 32768
-    net.core.netdev_max_backlog = 32768
-    net.ipv4.tcp_timestamps = 0
-    net.ipv4.tcp_max_orphans = 32768
-    net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
-    sysctl -p
-fi
+    if ! grep -q "^net.ipv4.tcp_congestion_control = bbr" /etc/sysctl.conf; then
+        echo "net.core.default_qdisc = fq
+        net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+        sysctl -p
+    fi
+    if ! grep -q "^net.nf_conntrack_max = 20971520" /etc/sysctl.conf; then
+        echo "fs.file-max = 1000000
+        fs.inotify.max_user_instances = 8192
+        net.core.somaxconn = 32768
+        net.core.netdev_max_backlog = 32768
+        net.ipv4.ip_forward = 1
+        net.ipv4.ip_local_port_range = 10000 65000
+        net.ipv4.route.gc_timeout = 100
+        net.ipv4.tcp_retries2 = 8
+        net.ipv4.tcp_slow_start_after_idle = 0
+        net.ipv4.tcp_syncookies = 1
+        net.ipv4.tcp_fin_timeout = 30
+        net.ipv4.tcp_tw_reuse = 1
+        net.ipv4.tcp_max_syn_backlog = 16384
+        net.ipv4.tcp_max_tw_buckets = 6000
+        net.ipv4.tcp_syn_retries = 1
+        net.ipv4.tcp_synack_retries = 1
+        net.ipv4.tcp_timestamps = 0
+        net.ipv4.tcp_max_orphans = 32768
+        net.nf_conntrack_max = 20971520" >> /etc/sysctl.conf
+        sysctl -p
+    fi
 sed -i "s/command: \[\"-c\", \".*\"\]/command: \[\"-c\", \"$relay_node_id\"\]/" docker-compose.yaml
 
 docker compose up -d
