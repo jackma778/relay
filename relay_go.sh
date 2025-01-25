bbe8d4c1a020a214a48479b87323e23ddbdd5382
jackma778 <36823039+jackma778@users.noreply.github.com>
2025-01-25 19:27:18 +0900

    Update relay_go.sh

--- relay_go.sh
+++ relay_go.sh
@@ -63,14 +63,14 @@ docker compose up -d
 docker cp ehco ehco:/usr/bin/ehco
 docker restart ehco
 sleep 3 
-if crontab -l | grep -q "ehco"; then
+if crontab -l | grep -q "ehco" && crontab -l | grep -q "iperf3"; then
     echo "pass"
 else
     echo "add crontab"
     minute=$(shuf -i 0-59 -n 1)
     hour=$(shuf -i 3-6 -n 1)
     weekday=$(shuf -i 0-6 -n 1)
-    (crontab -l ; echo "$minute $hour * * $weekday docker restart ehco") | crontab -
+    (crontab -l ; echo "$minute $hour * * $weekday docker restart ehco iperf3") | crontab -
 fi
 docker logs -n 10 ehco
 
